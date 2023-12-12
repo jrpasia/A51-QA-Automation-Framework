4abc80bf9b563437eafa68a1aab72df201ee8120
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-12-12 15:57:37 -0600

    Login test
    Steps included

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -20,6 +22,18 @@ public class LoginTests extends BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("john.pasia@testpro.io");
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("B3n@iah2013");
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      //  String home = "https://qa.koel.app/#!/home";
+       // Assert.assertEquals(driver.getCurrentUrl(),home);
         driver.quit();
     }
 }
