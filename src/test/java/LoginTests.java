477f50436742e9dc59b8ce5768cf0ea34cb81921
jrpasia <121219634+jrpasia@users.noreply.github.com>
2023-12-12 16:52:38 -0600

    login test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -22,18 +22,39 @@ public class LoginTests extends BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
 
+    @Test
+    public void loginValidPassword(){
+        //pre-conditions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Steps 1: Go to website
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        //Step 2: Find email text box
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
         emailField.click();
+        emailField.clear();
         emailField.sendKeys("john.pasia@testpro.io");
+
+        //Step 3: Find password text box
         WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
         passwordField.click();
+        passwordField.clear();
         passwordField.sendKeys("B3n@iah2013");
+
+        //Step 4: Find and click Login button
         WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
         loginButton.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-      //  String home = "https://qa.koel.app/#!/home";
-       // Assert.assertEquals(driver.getCurrentUrl(),home);
-        driver.quit();
+
+        //Assertion
+        WebElement userAvatar = driver.findElement(By.cssSelector("[class='avatar']"));
+        // driver.quit();
     }
 }
